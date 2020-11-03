939ae165a427dadb860547ff677708aeabe54b1f
George Rellias <lokarrcursed@protonmail.com>
2020-11-03 13:18:33 +0200

    Created the structure movie array .
    Created the movie, director_info and date structures along with the movies array and ran a few tests for the first functions.

---
+++ Movie-Manager.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+
+typedef struct date
+{
+    int day, month, year;
+} date;
+
+typedef struct director_info
+{
+    char* director_surname, director_name;
+} director_info;
+
+typedef struct movie
+{
+    int id;
+    char* title;
+    director_info* director;
+    date* release_date;
+} movie;
+
+void insert_movie();
+movie * delete_movie(movie *movie_collection, int id);
+
+/* Utility functions */
+
+//Binary search and bubble sort go here
+
+/* Utility functions */
+
+void main(int argc, char* argv[])
+{
+    movie movie_collection[1];
+    delete_movie(movie_collection,2);
+}
+
+void insert_movie()
+{
+    //if the array is full reallocate it's size
+    //take input from user
+    //calculate movie id
+    //add it to the array
+}
+
+movie * delete_movie(movie *movie_collection, int id)
+{
+    //use binary search to get the movie by id
+    //delete the movie
+    //return array
+}
